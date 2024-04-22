7c9c56d59fd73fa2043ba9ebce7dcda7e341aa6e
Fernando Gil <fernandogilvidal@gmail.com>
2024-04-22 23:16:04 +0200

    fluent validation

--- src/CleanArchitecture/CleanArchitecture.Application/DependencyInjection.cs
+++ src/CleanArchitecture/CleanArchitecture.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using CleanArchitecture.Application.Abstractions.Behaviours;
 using CleanArchitecture.Domain.Rents;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchitecture.Application;
@@ -12,7 +13,10 @@ public static class DependencyInjection
         {
             configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
             configuration.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+            configuration.AddOpenBehavior(typeof(ValidationBehaviour<,>));
         });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
         services.AddTransient<PriceService>();
         return services;
     }
