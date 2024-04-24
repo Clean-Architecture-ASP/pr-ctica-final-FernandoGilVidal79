1bb8d35417790b9f6acfb0c3525722528ebbedd3
Fernando Gil <fernandogilvidal@gmail.com>
2024-04-24 23:51:40 +0200

    Application DI

--- src/CleanArchitecture/CleanArchitecture.Api/Program.cs
+++ src/CleanArchitecture/CleanArchitecture.Api/Program.cs
@@ -1,3 +1,5 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 
