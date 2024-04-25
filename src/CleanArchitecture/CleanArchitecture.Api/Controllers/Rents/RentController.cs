8e6b73c2762a6808b40b1e0fa32494006d989abc
Fernando Gil <fernandogilvidal@gmail.com>
2024-04-25 18:50:05 +0200

    RentController

---
+++ src/CleanArchitecture/CleanArchitecture.Api/Controllers/Rents/RentController.cs
@@ -0,0 +1,45 @@
+using CleanArchitecture.Api.Controllers.Rents;
+using CleanArchitecture.Application.Rents.GetRent;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace CleanArchitecture.Api.Controllers;
+
+[ApiController]
+[Route("api/rents")]
+public class RentController : Controller
+{
+    private readonly ISender _sender;
+
+    public RentController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetRent (Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetRentQuery(id);
+        var result = await _sender.Send(query, cancellationToken);
+        return result.IsSuccess ? Ok(result.Value) : NotFound();
+    }
+
+    [HttpPost()]
+    public async Task<IActionResult> RentReservation(Guid id, RentReservationRequest rentRequest, CancellationToken cancellationToken)
+    {
+        var command = new RentReservationCommand(rentRequest.VehicleId, 
+                                        rentRequest.UserId, 
+                                        rentRequest.StartDate, rentRequest.EndDate);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailue)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return CreatedAtAction(nameof(GetRent), new {Id = result.Value}, result.Value);
+    }
+
+}
---
+++ src/CleanArchitecture/CleanArchitecture.Api/Controllers/Vehicles/VehiclesController.cs
@@ -0,0 +1,26 @@
+using CleanArchitecture.Application.Vehicles.SearchVehicles;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers.Vehicles;
+
+[ApiController]
+[Route("api/vehicles")]
+public class VehicleController : Controller
+{
+    private readonly ISender _sender;
+
+    public VehicleController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> SearchVehicles(DateOnly startDate, DateOnly endDate, CancellationToken cancellationToken)
+    {
+        var query = new  SearchVehiclesQuery(startDate,endDate);
+        var results = await _sender.Send(query, cancellationToken);
+        return Ok(results.Value);
+    }
+}
